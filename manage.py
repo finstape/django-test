352d6cbaedb12481ac1949715aea5d485a218c52
Artur Agishev <finstape@mail.ru>
2026-03-26 20:05:44 +0300

    fix: update noqa directive in manage.py import statement

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 def main() -> None:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.core.settings")
     try:
-        from django.core.management import execute_from_command_line  # noqa: F401
+        from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
         raise ImportError(  # noqa: TRY003
             "Couldn't import Django. Are you sure it's installed and "
